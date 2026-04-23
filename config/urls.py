325f9caad250481062a2a33b7443ad86f02a4e5b
iAngie28 <rosita.alvarez.blanco@gmail.com>
2026-04-23 01:09:38 -0400

    ruta revividor

--- config/urls.py
+++ config/urls.py
@@ -10,8 +10,8 @@ from usuarios.views.auth_views import LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/login/', LoginView.as_view(), name='login_global'),
-    path('health/', lambda r: HttpResponse("OK"), name='health')
-    
+    path('health/', lambda r: HttpResponse("OK"), name='health'),
+
     # DOCUMENTACIÓN API (drf-spectacular)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
