431bbc5c7e10acdab5c56a841d877d4ee4e6a441
iAngie28 <rosita.alvarez.blanco@gmail.com>
2026-04-28 22:28:56 -0400

    .

--- apps/users/serializers/auth_serializers.py
+++ apps/users/serializers/auth_serializers.py
@@ -1,4 +1,12 @@
 from rest_framework import serializers
+from apps.users.models.user import User
+
+class UserSerializer(serializers.ModelSerializer):
+    role_display = serializers.CharField(source='get_role_display', read_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'role', 'role_display']
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
--- apps/users/views/auth_views.py
+++ apps/users/views/auth_views.py
@@ -1,16 +1,27 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
 from rest_framework import status
+from apps.users.serializers.auth_serializers import UserSerializer
+from django.contrib.auth import authenticate
 
 class CustomLoginView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
+            user = authenticate(
+                username=request.data.get('username'),
+                password=request.data.get('password')
+            )
+            user_data = UserSerializer(user).data
+            
             return Response({
                 "success": True,
                 "message": "Login exitoso",
-                "data": serializer.validated_data
+                "data": {
+                    **serializer.validated_data,
+                    "user": user_data
+                }
             }, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({
